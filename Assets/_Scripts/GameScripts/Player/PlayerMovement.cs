a6d47c9c1c31b38c33dc09ba7dcd458edc9122c2
Mortadelegle <Mortadelegle@gmail.com>
2015-12-12 22:47:53 +0100

    Ditched Old Movement system

--- Assets/_Scripts/GameScripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/GameScripts/Player/PlayerMovement.cs
@@ -2,15 +2,13 @@
 using System.Collections;
 using AssemblyCSharp;
 
-public class PlayerMovement : MonoBehaviour, I_InputReceiver {
+public class PlayerMovement : MonoBehaviour {
 
     bool debug;
-    public Transform inputSender;
-
     [SerializeField]
-    private float m_speedDelta = 10;
+    private float m_speedDelta = 150;
     [SerializeField]
-    private float speedLoss = 1;
+    private float speedLoss = 30;
 
     private Rigidbody m_rb;
     private Transform m_tr;
@@ -19,73 +17,19 @@ public class PlayerMovement : MonoBehaviour, I_InputReceiver {
 
 	// Use this for initialization
 	void Start () {
-        inputSender.GetComponent<I_InputSender>().addInputReceiver(this);
         m_rb = GetComponent<Rigidbody>();
         m_tr = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        m_velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        m_velocity = m_velocity.normalized * Time.deltaTime * m_speedDelta;
+
+        print("Velocity is:" + m_velocity.x + m_velocity.y);
+
         m_rb.velocity = m_velocity;
-
-        m_tr.up = m_rb.velocity;
 	}
-
-    public void onAttachedToSender() {
-        if (debug)
-            Debug.Log("Attached to sender");
-    }
-
-    public void onDetachFromSender() {
-        if (debug)
-            Debug.Log("Detached from sender");
-    }
-
-    public void onButtonPressed(E_InputTypes type) {
-        switch (type) {
-            case E_InputTypes.Up:
-                m_velocity.y = m_speedDelta;
-                break;
-            case E_InputTypes.Down:
-                m_velocity.y = -m_speedDelta;
-                break;
-            case E_InputTypes.Left:
-                m_velocity.x = -m_speedDelta;
-                break;
-            case E_InputTypes.Right:
-                m_velocity.x = m_speedDelta;
-                break;
-        }
-    }
-
-    public void onButtonReleased(E_InputTypes type) {
-        switch (type) {
-            case E_InputTypes.Up:
-                if(Input.GetButton("Down"))
-                    m_velocity.y = -m_speedDelta;
-                else
-                    m_velocity.y = 0;
-            break;
-            case E_InputTypes.Down:
-                if(Input.GetButton("Up"))
-                    m_velocity.y = m_speedDelta;
-                else
-                    m_velocity.y = 0;
-            break;
-            case E_InputTypes.Left:
-                if(Input.GetButton("Right"))
-                    m_velocity.x = m_speedDelta;
-                else
-                    m_velocity.x = 0;
-            break;
-            case E_InputTypes.Right:
-                if(Input.GetButton("Left"))
-                    m_velocity.x = -m_speedDelta;
-                else
-                    m_velocity.x = 0;
-            break;
-        }
-    }
 
     public void slowDown() {
         m_speedDelta -= speedLoss;
