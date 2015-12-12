0a8320b7b93c6c0ced7ff65d2d497a797ed8079b
blorente <blorente@ucm.es>
2015-12-12 12:17:25 +0100

    Player Movement added

---
+++ Assets/_Scripts/GameScripts/Enemies/EnemyStateMachine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStateMachine : MonoBehaviour {
+
+    enum EnemyStates
+    {
+        Patrol, Preparing, Attacking, LookAround
+    }
+
+    private EnemyStates m_state;
+
+	// Use this forinitialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeState(EnemyStates newState)
+    {
+        onStateExit(m_state);
+        m_state = newState;
+        onStateEnter(m_state);
+    }
+
+    private void onStateEnter(EnemyStates m_state)
+    {
+        
+    }
+
+    private void onStateExit(EnemyStates m_state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
